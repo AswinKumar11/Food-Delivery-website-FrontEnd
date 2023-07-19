a17bc19cf2878f54bdf3943965d89d0eb4a307ec
AswinKumar11 <aswinyadav123@gmail.com>
2023-07-19 22:27:21 +0530

    test1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const heading = React.createElement("div", {id:'child'}, [
     React.createElement("h1", {key:'child2'}, "Hello Mr.React"),
-    React.createElement("h2", {key:'child3'}, "Hello Mr.Aswin")
+    React.createElement("h2", {key:'child3'}, "Hello Mr.Aswin"),
+    React.createElement("h2", {key:'child4'}, "Hello Mr.Aswin")
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
