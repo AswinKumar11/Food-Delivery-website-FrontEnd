25537efa0086bf2c3770958edff9b2587d6a0ae1
AswinKumar11 <aswinyadav123@gmail.com>
2023-08-18 23:40:41 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", {id:'child'}, [
     React.createElement("h1", {key:'child2'}, "Hello Mr.React"),
     React.createElement("h2", {key:'child3'}, "Hello Mr.Aswin"),
